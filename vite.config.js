2be00104f96d94503f3b82c2b9345b0b140f669a
Zoha Rehman <thezeeprogrammer@gmail.com>
2025-03-28 22:12:27 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.PURE_BRUSH || "/pure-brush-deploy"
 })
